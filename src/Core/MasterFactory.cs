80152636fa2f17e0601093af15f4e4b918e405cd
Paul Herrera <pherrera@dimensional.com>
2014-04-28 21:24:28 -0500

    Added some small comments.

--- src/Core/MasterFactory.cs
+++ src/Core/MasterFactory.cs
@@ -13,33 +13,49 @@ namespace Dimensional.TinyReturns.Core
         public static ISystemLog SystemLog { set { _systemLog = value; } }
         public static ISystemLog GetSystemLog() { return _systemLog; }
 
+        // **
+
         private static ITinyReturnsDatabaseSettings _tinyReturnsDatabaseSettings;
         public static ITinyReturnsDatabaseSettings TinyReturnsDatabaseSettings { set { _tinyReturnsDatabaseSettings = value; } }
         public static ITinyReturnsDatabaseSettings GetTinyReturnsDatabaseSettings() { return _tinyReturnsDatabaseSettings; }
+
+        // **
 
         private static IReturnsSeriesDataRepository _returnsSeriesDataRepository;
         public static IReturnsSeriesDataRepository ReturnsSeriesDataRepository { set { _returnsSeriesDataRepository = value; } }
         public static IReturnsSeriesDataRepository GetReturnsSeriesRepository() { return _returnsSeriesDataRepository; }
 
+        // **
+
         private static IMonthlyReturnsDataRepository _monthlyReturnsDataRepository;
         public static IMonthlyReturnsDataRepository MonthlyReturnsDataRepository { set { _monthlyReturnsDataRepository = value; } }
         public static IMonthlyReturnsDataRepository GetMonthlyReturnsDataRepository() { return _monthlyReturnsDataRepository; }
+
+        // **
 
         private static ICitiReturnsFileReader _citiReturnsFileReader;
         public static ICitiReturnsFileReader CitiReturnsFileReader { set { _citiReturnsFileReader = value; } }
         public static ICitiReturnsFileReader GetCitiReturnsFileReader() { return _citiReturnsFileReader; }
 
+        // **
+
         private static IInvestmentVehicleDataRepository _investmentVehicleDataRepository;
         public static IInvestmentVehicleDataRepository InvestmentVehicleDataRepository { set { _investmentVehicleDataRepository = value; } }
         public static IInvestmentVehicleDataRepository GetInvestmentVehicleDataRepository() { return _investmentVehicleDataRepository; }
+
+        // **
 
         private static IPerformanceReportExcelReportView _performanceReportExcelReportView;
         public static IPerformanceReportExcelReportView PerformanceReportExcelReportView { set { _performanceReportExcelReportView = value; } }
         public static IPerformanceReportExcelReportView GetPerformanceReportExcelReportView() { return _performanceReportExcelReportView; }
 
+        // **
+
         private static IFlatFileIo _flatFileIo;
         public static IFlatFileIo FlatFileIo { set { _flatFileIo = value; } }
         public static IFlatFileIo GetFlatFileIo() { return _flatFileIo; }
+
+        // **
 
         public static ICitiFileImportInteractor GetCitiFileImportInteractor()
         {
