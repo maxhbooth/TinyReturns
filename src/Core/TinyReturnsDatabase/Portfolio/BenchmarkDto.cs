5346e1a412afde525a68805247291226e4695771
Paul Herera <pherrera@dimensional.com>
2017-04-27 21:36:34 -0500

    Added DTOS

---
+++ src/Core/TinyReturnsDatabase/Portfolio/BenchmarkDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Dimensional.TinyReturns.Core.TinyReturnsDatabase.Portfolio
+{
+    public class BenchmarkDto
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public int ReturnSeriesId { get; set; }
+    }
+}
---
+++ src/Core/TinyReturnsDatabase/Portfolio/PortfolioDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dimensional.TinyReturns.Core.TinyReturnsDatabase.Portfolio
+{
+    public class PortfolioDto
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public DateTime InceptionDate { get; set; }
+        public DateTime? CloseDate { get; set; }
+        public int? NetReturnSeriesId { get; set; }
+        public int? GrossReturnSeriesId { get; set; }
+    }
+}
