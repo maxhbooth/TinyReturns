118173935bda2fbe7f42c107ab2b8911b7ed7682
Paul Herera <pherrera@dimensional.com>
2017-04-27 22:04:44 -0500

    Added test sdata

---
+++ data/mssql/TinyReturns/Up/0002_AddTestData.EVN.LOCAL.sql
@@ -0,0 +1,150 @@
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10000, N'Benchmark 00', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10001, N'Benchmark 01', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10002, N'Benchmark 02', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10003, N'Benchmark 03', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10004, N'Benchmark 04', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10005, N'Benchmark 05', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10006, N'Benchmark 06', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10007, N'Benchmark 07', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10008, N'Benchmark 08', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10009, N'Benchmark 09', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10010, N'Benchmark 10', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10011, N'Benchmark 11', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10012, N'Benchmark 12', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10013, N'Benchmark 13', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10014, N'Benchmark 14', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10015, N'Benchmark 15', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10016, N'Benchmark 16', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10017, N'Benchmark 17', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10018, N'Benchmark 18', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10019, N'Benchmark 19', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10020, N'Benchmark 20', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10021, N'Benchmark 21', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10022, N'Benchmark 22', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10023, N'Benchmark 23', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10024, N'Benchmark 24', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10025, N'Benchmark 25', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10026, N'Benchmark 26', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10027, N'Benchmark 27', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10028, N'Benchmark 28', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10029, N'Benchmark 29', NULL)
+GO
+INSERT [Portfolio].[Benchmark] ([Number], [Name], [ReturnSeriesId]) VALUES (10030, N'Benchmark 30', NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (100, N'Portfolio 100', CAST(N'2000-01-08 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (101, N'Portfolio 101', CAST(N'2010-08-09 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (102, N'Portfolio 102', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (103, N'Portfolio 103', CAST(N'1985-12-30 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (104, N'Portfolio 104', CAST(N'2000-08-15 00:00:00.000' AS DateTime), CAST(N'2017-12-05 00:00:00.000' AS DateTime), NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (105, N'Portfolio 105', CAST(N'1999-07-03 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (106, N'Portfolio 106', CAST(N'2003-04-09 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (107, N'Portfolio 107', CAST(N'2010-08-09 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (108, N'Portfolio 108', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (109, N'Portfolio 109', CAST(N'1985-12-30 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (110, N'Portfolio 110', CAST(N'2000-08-15 00:00:00.000' AS DateTime), CAST(N'2001-11-04 00:00:00.000' AS DateTime), NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (111, N'Portfolio 111', CAST(N'1999-07-03 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (112, N'Portfolio 112', CAST(N'2003-04-09 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (113, N'Portfolio 113', CAST(N'2010-08-09 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (114, N'Portfolio 114', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (115, N'Portfolio 115', CAST(N'1985-12-30 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (116, N'Portfolio 116', CAST(N'2000-08-15 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (117, N'Portfolio 117', CAST(N'1999-07-03 00:00:00.000' AS DateTime), CAST(N'2005-03-07 00:00:00.000' AS DateTime), NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (118, N'Portfolio 118', CAST(N'2003-04-09 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (119, N'Portfolio 119', CAST(N'2013-01-03 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[Portfolio] ([Number], [Name], [InceptionDate], [CloseDate], [NetReturnSeriesId], [GrossReturnSeriesId]) VALUES (120, N'Portfolio 120', CAST(N'2000-08-15 00:00:00.000' AS DateTime), NULL, NULL, NULL)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (101, 10010, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (102, 10000, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (102, 10005, 3)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (102, 10010, 2)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (103, 10022, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (104, 10003, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (105, 10004, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (106, 10005, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (107, 10006, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (108, 10007, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (109, 10008, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (110, 10009, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (111, 10010, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (112, 10020, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (113, 10021, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (114, 10022, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (115, 10023, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (115, 10028, 2)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (116, 10022, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (117, 10030, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (118, 10010, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (119, 10027, 1)
+GO
+INSERT [Portfolio].[PortfolioToBenchmark] ([PortfolioNumber], [BenchmarkNumber], [SortOrder]) VALUES (120, 10005, 1)
+GO
