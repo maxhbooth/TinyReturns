70abf32e0b327e6f967bb341ac939374076a8a39
Paul Herera <pherrera@dimensional.com>
2017-05-07 05:37:59 -0500

    Added ReturnSeriesDataGateway

---
+++ src/Database/TinyReturnsDatabase/Performance/ReturnSeriesDataGateway.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using Dapper;
+using Dimensional.TinyReturns.Core;
+using Dimensional.TinyReturns.Core.TinyReturnsDatabase.Performance;
+
+namespace Dimensional.TinyReturns.Database.TinyReturnsDatabase.Performance
+{
+    public class ReturnSeriesDataGateway : BaseTinyReturnsDataTableGateway, IReturnSeriesDataGateway
+    {
+        public ReturnSeriesDataGateway(
+            ITinyReturnsDatabaseSettings tinyReturnsDatabaseSettings,
+            ISystemLog systemLog)
+            : base(tinyReturnsDatabaseSettings, systemLog)
+        {
+        }
+
+        public ReturnSeriesDto[] GetAll()
+        {
+            const string sql = @"
+SELECT
+        [ReturnSeriesId]
+        ,[Name]
+        ,[Disclosure]
+    FROM
+        [Performance].[ReturnSeries]";
+
+            ReturnSeriesDto[] result = null;
+
+            ConnectionExecuteWithLog(
+                connection =>
+                {
+                    result = connection.Query<ReturnSeriesDto>(sql).ToArray();
+                },
+                sql);
+
+            return result;
+        }
+
+        public void Insert(ReturnSeriesDto[] dtos)
+        {
+            const string sql = @"
+INSERT INTO [Performance].[ReturnSeries]
+           ([Name]
+           ,[Disclosure])
+     VALUES
+           (@Name
+           , @Disclosure)
+";
+
+            ConnectionExecuteWithLog(
+                connection =>
+                {
+                    connection.Execute(sql, dtos);
+                },
+                sql,
+                dtos);
+        }
+    }
+}
