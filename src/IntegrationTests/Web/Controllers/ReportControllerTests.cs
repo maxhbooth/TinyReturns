9ee657703333a1c6231fe79387e34950051d478c
Paul Herera <pherrera@dimensional.com>
2017-05-07 20:08:41 -0500

    Added report controller

---
+++ src/IntegrationTests/Web/Controllers/ReportControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Dimensional.TinyReturns.Web.Controllers;
+using Xunit;
+
+namespace Dimensional.TinyReturns.IntegrationTests.Web.Controllers
+{
+    public class ReportControllerTests
+    {
+        public class TestHelper
+        {
+            public ReportController CreateController()
+            {
+                return new ReportController();
+            }
+        }
+
+        [Fact]
+        public void ShouldWork()
+        {
+            var testHelper = new TestHelper();
+
+            var controller = testHelper.CreateController();
+
+            var actionResult = controller.Index();
+
+            Assert.Null(actionResult);
+        }
+    }
+}
---
+++ src/Web/Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Dimensional.TinyReturns.Web.Controllers
+{
+    public class ReportController : Controller
+    {
+        public ActionResult Index()
+        {
+            return null;
+        }
+    }
+}
