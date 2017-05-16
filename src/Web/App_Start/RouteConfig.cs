3cb75a7fb0261331fe57e384dd94bf131c91e461
Paul Herera <pherrera@dimensional.com>
2017-05-16 17:05:24 -0500

    Web application now works

--- src/Web/App_Start/RouteConfig.cs
+++ src/Web/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace Dimensional.TinyReturns.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Report", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "PortfolioPerformance", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
