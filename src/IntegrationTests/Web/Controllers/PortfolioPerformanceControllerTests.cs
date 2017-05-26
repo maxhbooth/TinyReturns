cce038db80f91c0ebcb6065a63b5cef5742b83f8
Max Booth <Max.Booth@dimensional.com>
2017-05-26 15:19:58 -0500

    worked on tests

--- src/IntegrationTests/Web/Controllers/PortfolioPerformanceControllerTests.cs
+++ src/IntegrationTests/Web/Controllers/PortfolioPerformanceControllerTests.cs
@@ -272,7 +272,7 @@ namespace Dimensional.TinyReturns.IntegrationTests.Web.Controllers
         }
 
         [Fact]
-        public void ShouldReturnSinglePortfolioWithReturnsForAllValues()
+        public void ShouldReturnSinglePortfolioWithReturnsForFourMonths()
         {
             // Arrange
             var testHelper = new TestHelper();
@@ -370,6 +370,143 @@ namespace Dimensional.TinyReturns.IntegrationTests.Web.Controllers
 
                 viewResultModel[0].OneMonth.Should().BeApproximately(0.02m, 0.00000001m);
                 viewResultModel[0].ThreeMonth.Should().BeApproximately(0.039584m, 0.00000001m);
+                viewResultModel[0].YearToDate.Should().BeApproximately(0.0394800416m, 0.00000001m);
+            });
+        }
+
+
+
+        [Fact]
+        public void ShouldReturnSinglePortfolioWithReturnsForSevenMonths()
+        {
+            // Arrange
+            var testHelper = new TestHelper();
+
+            testHelper.DatabaseDataDeleter(() =>
+            {
+                var portfolioNumber = 100;
+                var portfolioName = "Portfolio 100";
+
+                var monthYear = new MonthYear(2016, 5);
+                var monthYearMinus1 = monthYear.AddMonths(-1);
+                var monthYearMinus2 = monthYear.AddMonths(-2);
+                var monthYearMinus3 = monthYear.AddMonths(-3);
+                var monthYearMinus4 = monthYear.AddMonths(-4);
+                var monthYearMinus5 = monthYear.AddMonths(-5);
+                var monthYearMinus6 = monthYear.AddMonths(-6);
+                var monthYearMinus7 = monthYear.AddMonths(-7);
+                var nextMonth = monthYear.AddMonths(1);
+
+                testHelper.CurrentDate = new DateTime(
+                    nextMonth.Year,
+                    nextMonth.Month,
+                    5);
+
+                testHelper.InsertPortfolioDto(new PortfolioDto()
+                {
+                    Number = portfolioNumber,
+                    Name = portfolioName,
+                    InceptionDate = new DateTime(2010, 1, 1)
+                });
+
+                var returnSeriesId = testHelper.InsertReturnSeriesDto(new ReturnSeriesDto()
+                {
+                    Name = "Return Series for Portfolio 100",
+                    Disclosure = string.Empty
+                });
+
+                testHelper.InsertPortfolioToReturnSeriesDto(new PortfolioToReturnSeriesDto()
+                {
+                    PortfolioNumber = portfolioNumber,
+                    ReturnSeriesId = returnSeriesId,
+                    SeriesTypeCode = PortfolioToReturnSeriesDto.NetSeriesTypeCode
+                });
+
+                testHelper.InsertMonthlyReturnDto(new MonthlyReturnDto()
+                {
+                    ReturnSeriesId = returnSeriesId,
+                    Year = monthYearMinus7.Year,
+                    Month = monthYearMinus7.Month,
+                    ReturnValue = 0.01m
+                });
+
+                testHelper.InsertMonthlyReturnDto(new MonthlyReturnDto()
+                {
+                    ReturnSeriesId = returnSeriesId,
+                    Year = monthYearMinus6.Year,
+                    Month = monthYearMinus6.Month,
+                    ReturnValue = 0.02m
+                });
+
+                testHelper.InsertMonthlyReturnDto(new MonthlyReturnDto()
+                {
+                    ReturnSeriesId = returnSeriesId,
+                    Year = monthYearMinus5.Year,
+                    Month = monthYearMinus5.Month,
+                    ReturnValue = 0.03m
+                });
+
+                testHelper.InsertMonthlyReturnDto(new MonthlyReturnDto()
+                {
+                    ReturnSeriesId = returnSeriesId,
+                    Year = monthYearMinus4.Year,
+                    Month = monthYearMinus4.Month,
+                    ReturnValue = -0.01m
+                });
+
+                testHelper.InsertMonthlyReturnDto(new MonthlyReturnDto()
+                {
+                    ReturnSeriesId = returnSeriesId,
+                    Year = monthYearMinus3.Year,
+                    Month = monthYearMinus3.Month,
+                    ReturnValue = 0.01m
+                });
+
+                testHelper.InsertMonthlyReturnDto(new MonthlyReturnDto()
+                {
+                    ReturnSeriesId = returnSeriesId,
+                    Year = monthYearMinus2.Year,
+                    Month = monthYearMinus2.Month,
+                    ReturnValue = 0.04m
+                });
+
+                testHelper.InsertMonthlyReturnDto(new MonthlyReturnDto()
+                {
+                    ReturnSeriesId = returnSeriesId,
+                    Year = monthYearMinus1.Year,
+                    Month = monthYearMinus1.Month,
+                    ReturnValue = -0.02m
+                });
+
+                testHelper.InsertMonthlyReturnDto(new MonthlyReturnDto()
+                {
+                    ReturnSeriesId = returnSeriesId,
+                    Year = monthYear.Year,
+                    Month = monthYear.Month,
+                    ReturnValue = 0.02m
+                });
+
+                var controller = testHelper.CreateController();
+
+                // Act
+                var actionResult = controller.Index();
+
+                // Assert
+                var viewResultModel = GetModelFromActionResult(actionResult);
+
+                viewResultModel.Length.Should().Be(1);
+
+                viewResultModel[0].Number.Should().Be(portfolioNumber);
+                viewResultModel[0].Name.Should().Be(portfolioName);
+                viewResultModel[0].Benchmarks.Should().BeEmpty();
+
+                var ExpectedThreeMonthResult = (1.02m) * (.98m) * (1.04m) - 1;
+                var ExpectedSixMonthResult = (1.02m) * (.98m) * (1.04m) * (1.01m) * (.99m) * (1.03m) - 1;
+
+                viewResultModel[0].OneMonth.Should().BeApproximately(0.02m, 0.00000001m);
+                viewResultModel[0].ThreeMonth.Should().BeApproximately(ExpectedThreeMonthResult, 0.00000001m);
+                viewResultModel[0].SixMonth.Should().BeApproximately(
+                ExpectedSixMonthResult, 0.00000001m);
                 viewResultModel[0].YearToDate.Should().BeApproximately(0.0394800416m, 0.00000001m);
             });
         }
