79ae583be67230a86424bc9207cde3d97b9ed70d
Paul Herera <pherrera@dimensional.com>
2017-05-28 19:34:02 -0500

    Added missing database relationships

--- data/mssql/TinyReturns/Up/0001_CreateTables.sql
+++ data/mssql/TinyReturns/Up/0001_CreateTables.sql
@@ -118,3 +118,19 @@ CREATE TABLE [Performance].[BenchmarkToReturnSeries](
 	[ReturnSeriesId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+
+ALTER TABLE [Performance].[BenchmarkToReturnSeries]  WITH CHECK ADD  CONSTRAINT [FK_BenchmarkToReturnSeries_ReturnSeries] FOREIGN KEY([ReturnSeriesId])
+REFERENCES [Performance].[ReturnSeries] ([ReturnSeriesId])
+GO
+
+ALTER TABLE [Performance].[BenchmarkToReturnSeries] CHECK CONSTRAINT [FK_BenchmarkToReturnSeries_ReturnSeries]
+GO
+
+ALTER TABLE [Performance].[PortfolioToReturnSeries]  WITH CHECK ADD  CONSTRAINT [FK_PortfolioToReturnSeries_ReturnSeries] FOREIGN KEY([ReturnSeriesId])
+REFERENCES [Performance].[ReturnSeries] ([ReturnSeriesId])
+GO
+
+ALTER TABLE [Performance].[PortfolioToReturnSeries] CHECK CONSTRAINT [FK_PortfolioToReturnSeries_ReturnSeries]
+GO
+
+
