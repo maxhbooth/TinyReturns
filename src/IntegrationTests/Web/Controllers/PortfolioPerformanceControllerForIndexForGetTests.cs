8b0826276cd3f731b839d9ea03360c4d24cd8251
Paul Herera <pherrera@dimensional.com>
2017-06-05 13:46:52 -0500

    Separated out test helper

--- src/IntegrationTests/Web/Controllers/PortfolioPerformanceControllerForIndexForGetTests.cs
+++ src/IntegrationTests/Web/Controllers/PortfolioPerformanceControllerForIndexForGetTests.cs
@@ -2,165 +2,24 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Web.Mvc;
-using Dimensional.TinyReturns.Core.PortfolioReportingContext.Domain;
 using Dimensional.TinyReturns.Core.PortfolioReportingContext.Services.PublicWebReport;
 using Dimensional.TinyReturns.Core.SharedContext.Services.DateExtend;
 using Dimensional.TinyReturns.Core.SharedContext.Services.TinyReturnsDatabase.Performance;
 using Dimensional.TinyReturns.Core.SharedContext.Services.TinyReturnsDatabase.Portfolio;
-using Dimensional.TinyReturns.Database.TinyReturnsDatabase.Performance;
-using Dimensional.TinyReturns.Database.TinyReturnsDatabase.Portfolio;
 using Dimensional.TinyReturns.IntegrationTests.Core;
-using Dimensional.TinyReturns.Web.Controllers;
 using Dimensional.TinyReturns.Web.Models;
 using FluentAssertions;
 using Xunit;
 
 namespace Dimensional.TinyReturns.IntegrationTests.Web.Controllers
 {
-    public class PortfolioPerformanceControllerTests
+    public class PortfolioPerformanceControllerForIndexForGetTests
     {
-        public class TestHelper
-        {
-            private readonly AllTablesDeleter _allTablesDeleter;
-            private readonly PortfolioDataTableGateway _portfolioDataTableGateway;
-            private readonly ReturnSeriesDataTableGateway _returnSeriesDataTableGateway;
-            private readonly MonthlyReturnDataTableGateway _monthlyReturnDataTableGateway;
-            private readonly PortfolioToReturnSeriesDataTableGateway _portfolioToReturnSeriesDataTableGateway;
-            private readonly BenchmarkDataTableGateway _benchmarkDataTableGateway;
-            private readonly BenchmarkToReturnSeriesDataTableGateway _benchmarkToReturnSeriesDataTableGateway;
-            private readonly PortfolioToBenchmarkDataTableGateway _portfolioToBenchmarkDataTableGateway;
-
-            public TestHelper()
-            {
-                var databaseSettings = new DatabaseSettings();
-                var systemLogForIntegrationTests = new SystemLogForIntegrationTests();
-
-                CurrentDate = new DateTime(2017, 2, 3);
-
-                _allTablesDeleter = new AllTablesDeleter();
-
-                _portfolioDataTableGateway = new PortfolioDataTableGateway(
-                    databaseSettings,
-                    systemLogForIntegrationTests);
-
-                _benchmarkDataTableGateway = new BenchmarkDataTableGateway(
-                    databaseSettings,
-                    systemLogForIntegrationTests);
-
-                _returnSeriesDataTableGateway = new ReturnSeriesDataTableGateway(
-                    databaseSettings,
-                    systemLogForIntegrationTests);
-
-                _monthlyReturnDataTableGateway = new MonthlyReturnDataTableGateway(
-                    databaseSettings,
-                    systemLogForIntegrationTests);
-
-                _portfolioToReturnSeriesDataTableGateway = new PortfolioToReturnSeriesDataTableGateway(
-                    databaseSettings,
-                    systemLogForIntegrationTests);
-
-                _benchmarkToReturnSeriesDataTableGateway = new BenchmarkToReturnSeriesDataTableGateway(
-                    databaseSettings,
-                    systemLogForIntegrationTests);
-
-                _portfolioToBenchmarkDataTableGateway = new PortfolioToBenchmarkDataTableGateway(
-                    databaseSettings,
-                    systemLogForIntegrationTests);
-            }
-
-            public DateTime CurrentDate { get; set; }
-
-            public PortfolioPerformanceController CreateController()
-            {
-                var returnSeriesRepository = new ReturnSeriesRepository(
-                    _returnSeriesDataTableGateway,
-                    _monthlyReturnDataTableGateway);
-
-                var benchmarkWithPerformanceRepository = new BenchmarkWithPerformanceRepository(
-                    _benchmarkDataTableGateway,
-                    _benchmarkToReturnSeriesDataTableGateway,
-                    returnSeriesRepository);
-
-                var portfolioWithPerformanceRepository = new PortfolioWithPerformanceRepository(
-                    _portfolioDataTableGateway,
-                    _portfolioToReturnSeriesDataTableGateway,
-                    _portfolioToBenchmarkDataTableGateway,
-                    returnSeriesRepository,
-                    benchmarkWithPerformanceRepository);
-
-                var publicWebReportFacade = new PublicWebReportFacade(
-                    portfolioWithPerformanceRepository, 
-                    new ClockStub(CurrentDate));
-
-                return new PortfolioPerformanceController(
-                    publicWebReportFacade);
-            }
-
-            public void InsertPortfolioDto(
-                PortfolioDto dto)
-            {
-                _portfolioDataTableGateway.Insert(dto);
-            }
-
-            public void InsertBenchmarkDto(
-                BenchmarkDto dto)
-            {
-                _benchmarkDataTableGateway.Insert(new []{ dto });
-            }
-
-            public int InsertReturnSeriesDto(ReturnSeriesDto dto)
-            {
-                return _returnSeriesDataTableGateway.Insert(dto);
-            }
-
-            public void InsertMonthlyReturnDto(MonthlyReturnDto dto)
-            {
-                _monthlyReturnDataTableGateway.Insert(new []{ dto });
-            }
-
-            public void InsertMonthlyReturnDtos(MonthlyReturnDto[] dtos)
-            {
-                _monthlyReturnDataTableGateway.Insert(dtos);
-            }
-
-            public void InsertPortfolioToReturnSeriesDto(PortfolioToReturnSeriesDto dto)
-            {
-                _portfolioToReturnSeriesDataTableGateway.Insert(new []{ dto });
-            }
-
-            public void InsertBenchmarkToReturnSeriesDto(BenchmarkToReturnSeriesDto dto)
-            {
-                _benchmarkToReturnSeriesDataTableGateway.Insert(new []{ dto });
-            }
-
-            public void InsertPortfolioToBenchmarkDto(
-                PortfolioToBenchmarkDto dto)
-            {
-                _portfolioToBenchmarkDataTableGateway.Insert(new []{ dto });
-            }
-
-            public void DatabaseDataDeleter(
-                Action act)
-            {
-                var databaseSettings = new DatabaseSettings();
-
-                _allTablesDeleter.DeleteAllDataFromTables(
-                    databaseSettings.TinyReturnsDatabaseConnectionString,
-                    new AllTablesDeleter.TableInfoDto[0]);
-
-                act();
-
-                _allTablesDeleter.DeleteAllDataFromTables(
-                    databaseSettings.TinyReturnsDatabaseConnectionString,
-                    new AllTablesDeleter.TableInfoDto[0]);
-            }
-        }
-
         [Fact]
         public void ShouldReturnNoRecrodsWhenNoPortfolioAreFound()
         {
             // Arrange
-            var testHelper = new TestHelper();
+            var testHelper = new PortfolioPerformanceControllerTestHelper();
 
             testHelper.DatabaseDataDeleter(() =>
                 {
@@ -206,7 +65,7 @@ namespace Dimensional.TinyReturns.IntegrationTests.Web.Controllers
         public void ShouldReturnSinglePortfolioWithoutPerformanceNumbers()
         {
             // Arrange
-            var testHelper = new TestHelper();
+            var testHelper = new PortfolioPerformanceControllerTestHelper();
 
             testHelper.DatabaseDataDeleter(() =>
             {
@@ -240,7 +99,7 @@ namespace Dimensional.TinyReturns.IntegrationTests.Web.Controllers
         public void ShouldReturnSinglePortfolioSingleNetMonthOfPerformance()
         {
             // Arrange
-            var testHelper = new TestHelper();
+            var testHelper = new PortfolioPerformanceControllerTestHelper();
 
             testHelper.DatabaseDataDeleter(() =>
             {
@@ -311,7 +170,7 @@ namespace Dimensional.TinyReturns.IntegrationTests.Web.Controllers
         public void ShouldReturnSinglePortfolioWithReturnsForfiveMonths()
         {
             // Arrange
-            var testHelper = new TestHelper();
+            var testHelper = new PortfolioPerformanceControllerTestHelper();
 
             testHelper.DatabaseDataDeleter(() =>
             {
@@ -418,7 +277,7 @@ namespace Dimensional.TinyReturns.IntegrationTests.Web.Controllers
         public void ShouldReturnSinglePortfolioWithReturnsForEightMonths()
         {
             // Arrange
-            var testHelper = new TestHelper();
+            var testHelper = new PortfolioPerformanceControllerTestHelper();
             testHelper.DatabaseDataDeleter(() =>
             {
                 var portfolioNumber = 100;
@@ -559,7 +418,7 @@ namespace Dimensional.TinyReturns.IntegrationTests.Web.Controllers
         public void ShouldReturnPortfolioWithBenchmark()
         {
             // Arrange
-            var testHelper = new TestHelper();
+            var testHelper = new PortfolioPerformanceControllerTestHelper();
 
             testHelper.DatabaseDataDeleter(() =>
             {
