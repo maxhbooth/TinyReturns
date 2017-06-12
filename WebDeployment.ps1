cb6323b9ce058a3d57813734655766ef5566b287
Max Booth <Max.Booth@dimensional.com>
2017-06-12 14:00:15 -0500

    test

--- WebDeployment.ps1
+++ WebDeployment.ps1
@@ -1,4 +1,27 @@
-﻿#set-item wsman:\localhost\client\trustedhosts astof-retcul02d
+﻿#Get-Job | Remove-Job | out-null
 
-#Enter-PSSession -ComputerName astof-retcal02d -Credential mbooth
-hostname
+#$job = Start-Job -ScriptBlock { Enter-PSSession -ComputerName astof-retcal02d }
+#$job | Wait-Job 
+#Receive-Job $job
+
+#$job = Invoke-Command -ScriptBlock { Enter-PSSession -ComputerName astof-retcal02d }
+
+#$job = Invoke-Command -ScriptBlock { Enter-PSSession $sess} -ComputerName $hostname -AsJob
+#$job | Wait-Job 
+#sl c:\
+#cd \
+#hostnamejo
+Get-PSSession | Remove-PSSession
+$sess = New-PSSession -ComputerName astof-retcal02d 
+
+
+
+Invoke-Command -Session $sess -Scriptblock {$test = "team city works"
+
+
+
+
+
+}
+
+#Enter-PSSession $sess
