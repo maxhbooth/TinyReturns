5e5971e1afd18372c125fa22cd8ba28bb0b38ce1
Max Booth <Max.Booth@dimensional.com>
2017-06-13 11:14:03 -0500

    edited iis deployment

--- WebDeployment.ps1
+++ WebDeployment.ps1
@@ -53,10 +53,12 @@ cd IIS:\Sites\
 
 if (Test-Path $iisAppName -pathType container)
 {
+    rm $iisAppName
     return
+    
 }
 
-$iisApp = New-Item $iisAppName -bindings @{protocol="http *";bindingInformation=":1704:"} -physicalPath $directoryPath
+$iisApp = New-Item $iisAppName -bindings @{protocol="http";bindingInformation=":1704:"+ $iisAppName} -physicalPath $directoryPath
 
 
 }
