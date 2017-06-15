344f571dd3e1e96ff445d38921226ed323c4830b
Max Booth <Max.Booth@dimensional.com>
2017-06-15 08:24:56 -0500

    changed sql script

--- data/mssql/TinyReturns/Up/0003_addCountries.sql
+++ data/mssql/TinyReturns/Up/0003_addCountries.sql
@@ -1,6 +1,6 @@
 /****** Object:  Table [Portfolio].[Countries]    Script Date: 4/15/2017 7:37:24 AM ******/
 CREATE TABLE [Portfolio].[Countries](
-	[CountryId] [int] NOT NULL,
+	[CountryId] [int] NOT NULL DEFAULT 0,
 	[CountryName] [nvarchar](255) NOT NULL,
 	[DateCreated] [datetime] NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	[DateUpdated] [datetime] NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -24,11 +24,12 @@ GO
 INSERT [Portfolio].[Countries] ([CountryId], [CountryName]) VALUES (3, N'United States')
 GO
 
-
+/*
 ALTER TABLE [Portfolio].[Portfolio]  WITH CHECK ADD CONSTRAINT [FK_Portfolio_Country] FOREIGN KEY([CountryId])
 REFERENCES [Portfolio].[Countries] ([CountryId])
 GO
 
 ALTER TABLE [Portfolio].[Portfolio] CHECK CONSTRAINT [FK_Portfolio_Country]
 GO
+*/
 
