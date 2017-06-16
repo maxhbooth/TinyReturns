947cbc51813b51c9033edbcae39c143d542762f0
Max Booth <Max.Booth@dimensional.com>
2017-06-16 16:19:50 -0500

    ajax?

---
+++ src/Web/Views/PortfolioPerformance/ajaxCalls.js
@@ -0,0 +1,31 @@
+﻿function UpdatePortfolio(portfolio) {
+
+
+    $.ajax({
+        url: 'http://localhost:52899/api/UpdatePortfolio',
+        type: 'POST',
+        data: JSON.stringify(portfolio),
+        dataType: "json",
+        success: function(data) {
+            alert("worked");
+        },
+        error: function(data) {
+            alert("error");
+        }
+    });
+}
+
+function createPortfolio(name, number, benchmarks) {
+    portfolio = {
+        Name: name,
+        Number: number,
+        Benchmarks: benchmarks
+    }
+
+    return portfolio;
+}
+
+
+
+
+}
